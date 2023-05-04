6b21f6be819baca99598c78a360278d34210c5eb
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-05-04 16:48:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,129 +1,130 @@
-import easyocr as ocr  # OCR
-import streamlit as st  # Web App
-from PIL import Image  # Image Processing
-import numpy as np  # Image Processing
-import gtts
+import streamlit as st
+from PIL import Image
+import io
 import base64
-from ocr import add_space
+import os
+import json
+import numpy as np
+import uuid
+import re
 
-page_bg_img = """
-<style>
-    [data-testid="stAppViewContainer"]{
-        background-image: linear-gradient(294deg, #07062E,#225580 0%,#1C104E);
-    }
-</style>
-"""
+import boto3
+import json
 
-css = """
-.css-1x8cf1d {
-    # background-color : #29e319;
-    padding: 12px 35px;
-    border-radius: 30px;
-    background-image: linear-gradient(87deg,#82829A,#363557);
-    border: unset;
-    outline: unset;
-}
-.css-1x8cf1d:hover{
-    color : #29e319;
-    background-image: linear-gradient(87deg,#363557,#82829A);
-}
-.row-widget{
-    text-align: center;
-}
-.css-81oif8 {
-    color: white;
-}
-.css-fg4pbf{
-    color: white;  
-}
-.css-rvekum{
-    color: white !important;
-}
-.st-dc{
-    color: white;
-}
-"""
+DEFAULT_REGION = "us-east-1"
+DEMO_IMAGE = "smart_meter.jpg"
 
-# or `visibility: hidden;`
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", DEFAULT_REGION)
 
-# st.markdown(f'<img src="text extraction from image/text_extract_proj/image/TechSmith-Blog-ExtractText.png" class="img-fluid">', unsafe_allow_html=True)
-image = Image.open('image/TechSmith-Blog-ExtractText.png')
-st.image(image, width=120) 
+client = boto3.client("rekognition", region_name=AWS_DEFAULT_REGION)
 
-st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-# title
-# st.title("Text Extraction from Images")
-page_title = '<p style="font-family:sans-serif; color: #29e319; font-size: 42px;">Text Extraction from Images</p>'
-st.markdown(page_title, unsafe_allow_html=True)
+def download_button(object_to_download, download_filename, button_text):
+    """
+    Generates a link to download the given object_to_download.
 
-# subtitle
-#st.markdown("## Text Extraction From Image")
+    Params:
+    ------
+    object_to_download:  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. my.json,
+    some_txt_output.txt download_link_text (str): Text to display for download
+    link.
+    button_text (str): Text to display on download button (e.g. 'click here to download file')
 
-st.markdown(page_bg_img,unsafe_allow_html=True)
+    Returns:
+    -------
+    (str): the anchor tag to download object_to_download
 
-# image uploader
-# st.markdown(Browse file ,unsafe_allow_html=True)
-image = st.file_uploader(label="Upload your image here",
-                         type=['png', 'jpg', 'jpeg'])
+    Examples:
+    --------
+    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+
+    """
+    object_to_download = json.dumps(object_to_download, ensure_ascii=True, indent=4)
+    try:
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
+
+    button_uuid = str(uuid.uuid4()).replace("-", "")
+    button_id = re.sub("\d+", "", button_uuid)
+
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
+
+    dl_link = (
+        custom_css
+        + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+    )
+
+    return dl_link
+
+
+def pil_image_to_byte_array(image):
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, "PNG")
+    return imgByteArr.getvalue()
+
 
 @st.cache
-def load_model():
-    reader = ocr.Reader(['en','hi'], model_storage_directory='.')
-    return reader
+def process_image(image_bytes):
+    response = client.detect_text(Image={"Bytes": image_bytes})
+    return response
 
-reader = load_model()  # load model
-final_text = None
-if image is not None:
-    input_image = Image.open(image)  # read image
-    st.image(input_image)  # display image
 
-    with st.spinner("🤖 AI is at Work! "): # Run spinner while extracting text.
+st.title("Extract text with AWS rekogniton text extraction")
+img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-        result = reader.readtext(np.array(input_image))
 
-        result_text = []  # empty list for results
+if img_file_buffer is not None:
+    image_bytes = pil_image_to_byte_array(Image.open(img_file_buffer))
+    image_array = np.array(Image.open(img_file_buffer))
 
-        for text in result:
-            result_text.append(text[1])
-            #result_text.append("\n")
-        final_text = ' '.join(result_text)
-        final_text = add_space(final_text)
-        st.success("Here you go! Below is the extracted text👇🏻")
-        st.code(final_text, language=None)
-        image_file_name = image.name if image.name else st.empty()
-        st.download_button(
-            label="Download",
-            data=final_text,
-            file_name="extracted_text_from_" + image_file_name + ".txt"
-        )
-    # Text to Audio part......................................................................
-    # Supporting variables
-    form_holder = st.empty()
-    yes_text = "Yes, I want to hear the Audio."
-    no_text = "No, I'm good."
-    with form_holder.form("Need audio"):
-        # st.write("Text to Audio")
-        text_audio = '<p style="font-family:sans-serif; color: #29e319; font-size: 42px;">Text to Audio</p>'
-        st.markdown(text_audio, unsafe_allow_html=True)
-
-        audio_required = st.radio("Do You wish to convert the text to audio?", 
-            (yes_text, no_text))
-
-        # Every form must have a submit button.
-        with st.spinner("🔊Preparing the audio.."):# Run spinner while converting text to speech.
-            
-            submitted = st.form_submit_button("Submit")
-            #form_holder.empty()
-            if submitted and audio_required == yes_text:
-                tts = gtts.gTTS(final_text)
-                audio_file_name = "my_audio.mp3"
-                tts.save(audio_file_name)
-                with open(audio_file_name, 'rb') as audio_file:
-                    audio_bytes = audio_file.read()
-                    st.audio(audio_bytes, format='audio/mp3')
-        #else:
-        #    form_holder.empty()
 else:
-    st.write("Upload an Image")
-    
+    image_bytes = open(DEMO_IMAGE, "rb").read()
+    image_array = np.array(Image.open(DEMO_IMAGE))
+
+response = process_image(image_bytes)
+extracted_text = [t["DetectedText"] for t in response["TextDetections"]]
+
+st.image(
+    image_array, use_column_width=True,
+)
+
+st.header("Extracted text")
+st.write(extracted_text)
+
+download_button_str = download_button(
+    extracted_text, "extracted_text.json", f"Click here to download extracted_text"
+)
+st.markdown(download_button_str, unsafe_allow_html=True)
+
+st.header("Raw response")
+st.write(response)
